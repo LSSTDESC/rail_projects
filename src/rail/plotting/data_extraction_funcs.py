cd8b33ded7fda5ace5dadaa482298915c7204c93
Tianqing Zhang <ztq1996@live.com>
2025-04-24 14:11:45 -0400

    update scatter plot, add stats vs. mag and stats vs. redshift, change data extraction funcs

--- src/rail/plotting/data_extraction_funcs.py
+++ src/rail/plotting/data_extraction_funcs.py
@@ -67,6 +67,31 @@ def extract_z_point(
     z_estimates = np.squeeze(qp_ens.ancil[colname])
     return z_estimates
 
+def extract_mag(
+    filepath: str,
+    colname: str = "mag_lsst_i",
+) -> np.ndarray:
+    """Extract the i-mag from a file
+
+    Parameters
+    ----------
+    filepath: str
+        Path to file with tabular data
+
+    colname: str
+        Name of the column with redshfits ['redshift']
+
+    Returns
+    -------
+    magnitude: np.ndarray
+        Magnitude in question
+
+    Notes
+    -----
+    This assumes the magnitude are in a file that can be read by tables_io
+    """
+    magnitude_table = tables_io.read(filepath)
+    return magnitude_table[colname]
 
 def extract_z_pdf(
     filepath: str,
@@ -144,6 +169,35 @@ def make_z_true_z_point_dict(
     )
     return out_dict
 
+def make_z_true_z_point_mag_dict(
+    z_true: np.ndarray,
+    z_estimate: np.ndarray,
+    mag: np.ndarray,
+) -> dict[str, np.ndarray]:
+    """Build a dictionary with true redshifts and a point_estimates
+
+    Parameters
+    ----------
+    z_true: np.ndarray
+        True Redshifts
+
+    z_estimate: np.ndarray
+        Point estimates
+
+    mag: np.ndarray
+        Magnitude
+        
+    Returns
+    -------
+    out_dict: dict[str, np.ndarray]
+        Dictionary with true redshift and a point estimate of the redshift
+    """
+    out_dict: dict[str, Any] = dict(
+        truth=z_true,
+        pointEstimate=z_estimate,
+        magnitude=mag
+    )
+    return out_dict
 
 def make_z_true_multi_z_point_dict(
     z_true: np.ndarray,
@@ -356,7 +410,8 @@ def get_pz_point_estimate_data(
         return None
     z_true_data = extract_z_true(z_true_path)
     z_estimate_data = extract_z_point(z_estimate_path)
-    pz_data = make_z_true_z_point_dict(z_true_data, z_estimate_data)
+    mag_data = extract_mag(z_true_path)
+    pz_data = make_z_true_z_point_dict(z_true_data, z_estimate_data, mag_data)
     return pz_data
 
 
