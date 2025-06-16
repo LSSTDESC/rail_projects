e5cdc6bebeda1d394cdc8a9ea1bb0a45351e24c1
Eric Charles <badass@stanford.edu>
2025-06-16 13:02:45 -0700

    Eac/dp1 tooling (#40)
    * Added wrap_model and fixed get_mag
    
    * switch outdir default
    
    * switch outdir default
    
    * Allow for missing outdir
    
    * fix yaml whitespace
    
    * Added DP1Reducer
    
    * remove cat_plotter stuff and fix imports
    
    * fix whitespace

--- src/rail/plotting/data_extraction_funcs.py
+++ src/rail/plotting/data_extraction_funcs.py
@@ -10,7 +10,9 @@ import numpy as np
 import qp
 import tables_io
 
-from rail.projects import RailProject
+from rail.utils.catalog_utils import CatalogConfigBase
+
+from rail.projects import RailProject, path_funcs
 
 
 def extract_z_true(
@@ -203,153 +205,6 @@ def make_z_true_multi_z_point_dict(
     return out_dict
 
 
-def get_z_true_path(
-    project: RailProject,
-    selection: str,
-    flavor: str,
-    tag: str,
-) -> str:
-    """Get the path to the the file with true redshfits
-    for a particualar analysis selection and flavor
-
-    Parameters
-    ----------
-    project: RailProject
-        Object with information about the structure of the current project
-
-    selection: str
-        Data selection in question, e.g., 'gold', or 'blended'
-
-    flavor: str
-        Analysis flavor in question, e.g., 'baseline' or 'zCosmos'
-
-    tag: str
-        File tag, e.g., 'test' or 'train', or 'train_zCosmos'
-
-    Returns
-    -------
-    path: str
-        Path to the file in question
-    """
-    return project.get_file_for_flavor(flavor, tag, selection=selection)
-
-
-def get_ceci_pz_output_path(
-    project: RailProject,
-    selection: str,
-    flavor: str,
-    algo: str,
-) -> str | None:
-    """Get the paths to the file with redshfit estimates
-    for a particualar analysis selection and flavor
-
-    Parameters
-    ----------
-    project: RailProject
-        Object with information about the structure of the current project
-
-    selection: str
-        Data selection in question, e.g., 'gold', or 'blended'
-
-    flavor: str
-        Analysis flavor in question, e.g., 'baseline' or 'zCosmos'
-
-    algo: str
-        Algorithm we want the estimates for, e.g., 'knn', 'bpz', etc..
-
-    Returns
-    -------
-    path: str | None
-        Path to the file in question, if it exists, otherwise None
-    """
-    outdir = project.get_path("ceci_output_dir", selection=selection, flavor=flavor)
-    basename = f"output_estimate_{algo}.hdf5"
-    outpath = os.path.join(outdir, basename)
-    return outpath if os.path.exists(outpath) else None
-
-
-def get_ceci_nz_output_paths(
-    project: RailProject,
-    selection: str,
-    flavor: str,
-    algo: str,
-    classifier: str,
-    summarizer: str,
-) -> list[str]:
-    """Get the paths to the file with n(z) estimates
-    for a particualar analysis selection and flavor
-
-    Parameters
-    ----------
-    project: RailProject
-        Object with information about the structure of the current project
-
-    selection: str
-        Data selection in question, e.g., 'gold', or 'blended'
-
-    flavor: str
-        Analysis flavor in question, e.g., 'baseline' or 'zCosmos'
-
-    algo: str
-        Algorithm we want the estimates for, e.g., 'knn', 'bpz'], etc...
-
-    classifier: str
-        Algorithm we use to make tomograpic bin
-
-    summarizer: str
-        Algorithm we use to go from p(z) to n(z)
-
-    Returns
-    -------
-    paths: list[str]
-        Paths to data
-    """
-    outdir = project.get_path("ceci_output_dir", selection=selection, flavor=flavor)
-    basename = f"single_NZ_summarize_{algo}_{classifier}_bin*_{summarizer}.hdf5"
-    outpath = os.path.join(outdir, basename)
-    paths = sorted(glob.glob(outpath))
-    return paths
-
-
-def get_ceci_true_nz_output_paths(
-    project: RailProject,
-    selection: str,
-    flavor: str,
-    algo: str,
-    classifier: str,
-) -> list[str]:
-    """Get the paths to the file with n(z) estimates
-    for a particualar analysis selection and flavor
-
-    Parameters
-    ----------
-    project: RailProject
-        Object with information about the structure of the current project
-
-    selection: str
-        Data selection in question, e.g., 'gold', or 'blended'
-
-    flavor: str
-        Analysis flavor in question, e.g., 'baseline' or 'zCosmos'
-
-    algo: str
-        Algorithm we want the estimates for, e.g., 'knn', 'bpz'], etc...
-
-    classifier: str
-        Algorithm we use to make tomograpic bin
-
-    Returns
-    -------
-    paths: list[str]
-        Paths to data
-    """
-    outdir = project.get_path("ceci_output_dir", selection=selection, flavor=flavor)
-    basename = f"true_NZ_true_nz_{algo}_{classifier}_bin*.hdf5"
-    outpath = os.path.join(outdir, basename)
-    paths = sorted(glob.glob(outpath))
-    return paths
-
-
 def get_pz_point_estimate_data(
     project: RailProject,
     selection: str,
@@ -382,15 +237,58 @@ def get_pz_point_estimate_data(
     pz_data: dict[str, np.ndarray] | None
         Data in question or None if a file is missing
     """
-    z_true_path = get_z_true_path(project, selection, flavor, tag)
-    z_estimate_path = get_ceci_pz_output_path(project, selection, flavor, algo)
+    z_true_path = path_funcs.get_z_true_path(project, selection, flavor, tag)
+    z_estimate_path = path_funcs.get_ceci_pz_output_path(project, selection, flavor, algo)
     if z_estimate_path is None:  # pragma: no cover
         return None
     z_true_data = extract_z_true(z_true_path)
     z_estimate_data = extract_z_point(z_estimate_path)
-    mag_data = extract_mag(z_true_path)
+    flavor_info = project.get_flavor(flavor)
+    catalog_tag = flavor_info['catalog_tag']
+    CatalogConfigBase.apply(catalog_tag)
+    catalog_class = CatalogConfigBase.active_class()
+    ref_band = catalog_class.band_template.format(band=catalog_class.ref_band)
+    mag_data = extract_mag(z_true_path, colname=ref_band)
     pz_data = make_z_true_z_point_dict(z_true_data, z_estimate_data, mag_data)
     return pz_data
+
+
+def get_ztrue_and_magntidues(
+    project: RailProject,
+    selection: str,
+    flavor: str,
+    tag: str,
+) -> dict[str, np.ndarray] | None:
+    """Get the true redshifts and observed magntidues
+    for a particualar analysis selection and flavor
+
+    Parameters
+    ----------
+    project: RailProject
+        Object with information about the structure of the current project
+
+    selection: str
+        Data selection in question, e.g., 'gold', or 'blended'
+
+    flavor: str
+        Analysis flavor in question, e.g., 'baseline' or 'zCosmos'
+
+    tag: str
+        File tag, e.g., 'test' or 'train', or 'train_zCosmos'
+
+    Returns
+    -------
+    out_data: dict[str, np.ndarray] | None
+        Data in question or None if a file is missing
+    """
+    z_true_path = path_funcs.get_z_true_path(project, selection, flavor, tag)
+    z_true_data = extract_z_true(z_true_path)
+    mag_data = extract_magntidues(z_true_path)
+    out_data = dict(
+        truth=z_true_data,
+        magntidues=mag_data,
+    )
+    return out_data
 
 
 def get_multi_pz_point_estimate_data(
@@ -468,7 +366,7 @@ def get_tomo_bins_nz_estimate_data(
     nz_data: qp.Ensemble
         Tomographic bin n(z) data
     """
-    paths = get_ceci_nz_output_paths(
+    paths = path_funcs.get_ceci_nz_output_paths(
         project,
         selection,
         flavor,
@@ -512,7 +410,7 @@ def get_tomo_bins_true_nz_data(
     nz_data: qp.Ensemble
         Tomographic bin n(z) data
     """
-    paths = get_ceci_true_nz_output_paths(
+    paths = path_funcs.get_ceci_true_nz_output_paths(
         project,
         selection,
         flavor,
